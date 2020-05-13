25fbe40f009773a782f2ea5aa13b2d5e54d9f7c2
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-05-13 01:35:34 -0500

    Cambios en las tarjetas de presentacion y comienzo de la seccion de portafolio

--- target.js
+++ target.js
@@ -1,5 +1,5 @@
-$('#button1').click( function (){ 
+/* $('#button1').click( function (){ 
   $('#button1').toggleClass('active');
   $('title').toggleClass('active');
   $('#nav1').toggleClass('active');
-});
+}); */
